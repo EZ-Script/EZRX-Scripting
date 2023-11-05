6db06572506f9571bc211660a18a4a4c28e35cf1
Christopher Diggins <cdiggins@gmail.com>
2023-11-05 08:55:18 -0500

    Creating scripting project

---
+++ EZRX.CSharp.Scripting/EZRX.CSharp.Scripting/StubProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZRX.CSharp.Scripting
+{
+    public static class StubProgram
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"Hello world");
+        }
+    }
+}
